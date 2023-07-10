3c5acbb41c1f67b292038b73da55b4dac52e1839
Talha Shahzad <123324616+talha-shahzad@users.noreply.github.com>
2023-07-10 20:42:06 +0500

    Update game.py

--- game.py
+++ game.py
@@ -1,71 +1,111 @@
-def analyzeboard():
-  return int(0)
-def CompTurn():
-  return 1
-def ConstBoard(board):
-  print("current state of the board \n\n")
+def analyzeboard(board):
+    cb = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    for i in range(0, 8):
+        if (board[cb[i][0]] != 0 and board[cb[i][0]] == board[cb[i][1]] and board[cb[i][1]] == board[cb[i][2]]):
+            return board[cb[i][0]];
+    return 0;
 
-  for i in range(0,9):
-    if((i>0) and (i%3==0)):
-      print('\n')
-    if(board[i]==0):
-      print('_ ')
-    if(board[i]==-1):
-      print('X ')
-    if(board[i]==1):
-      print('O ')
+
+def minmax(board, player):
+    x = analyzeboard(board)
+    if (x != 0):
+        return (x*player)
+    pos = -1
+    value = -2
+    for i in range(0, 9):
+        if (board[i] == 0):
+            board[i] = player;
+            score = -minmax(board, (player*-1))
+            board[i] = 0
+            if (score > value):
+                value = score
+                pos = i
+    if (pos == -1):
+        return 0;
+    return value;
+
+
+def CompTurn(board):
+    pos = -1
+    value = -2
+    for i in range(0, 9):
+        if (board[i] == 0):
+            board[i] = 1;
+            score = -minmax(board, -1)
+            board[i] = 0
+            if (score > value):
+                value = score
+                pos = i
+    board[pos] = 1
+
+
+def ConstBoard(board):
+    print("current state of the board \n\n")
+
+    for i in range(0, 9):
+        if ((i > 0) and (i % 3 == 0)):
+            print("\n")
+        if (board[i] == 0):
+            print("_", end=" ")
+        if (board[i] == -1):
+            print("X", end=" ")
+        if (board[i] == 1):
+            print('O', end=" ")
+
 
 def User1Turn(board):
-  pos=input('Enter a position for X from [1,2......9]');
-  pos=int(pos)-1;
-  if(board[pos]!=0):
-    print("Wrong Move")
-    exit(0);
-    board[pos-1]=-1
+    pos = input('Enter a position for X from [1,2......9]');
+    pos = int(pos);
+    if (board[pos] != 0):
+        print("Wrong Move")
+        exit(0);
+    board[pos-1] = -1
 
 
 def User2Turn(board):
-  pos=input('Enter a position for O from [1,2......9]');
-  pos=int(pos)-1;
-  if(board[pos]!=0):
-    print("Wrong Move")
-    exit(0);
-    board[pos-1]=1
+    pos = input('Enter a position for O from [1,2......9]');
+    pos = int(pos) - 1;
+    if (board[pos] != 0):
+        print("Wrong Move")
+        exit(0);
+    board[pos-1] = 1
 
 
 def main():
-  choice = input("enter 1 if you want to play single player , 2 for Multi player: ");
-  choice=int(choice)
-  board=[0, 0, 0, 0, 0, 0, 0, 0, 0]
-  if(choice==1):
-    print('you are playing against computer');
-    print('Computer: O and You: X');
-    player = input("press 1 if you want first turn else press 0");
-    player = int(player)
-    for i in range(0,9):
-      if(analyzeboard(board)!=0):
-        break;
-      if((i+player)%2==0):
-        CompTurn(board);
-      else:
-        ConstBoard(board);
-        User1Turn(board);
-    else:
-      for i in range(0,9):
-        if(analyzeboard(board)!=0):
-          break;
-        if(i%2==0):
-          CompTurn(board);
+    choice = input("enter 1 if you want to play single player , 2 for Multi player: ");
+    choice = int(choice)
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    if (choice == 1):
+        print('you are playing against computer');
+        print('Computer: O and You: X');
+        player = input("press 1 if you want first turn else press 0");
+        player = int(player)
+        for i in range(0, 9):
+            if (analyzeboard(board) != 0):
+                break;
+            if ((i+player) % 2 == 0):
+                CompTurn(board);
+            else:
+                ConstBoard(board);
+                User1Turn(board);
         else:
-          ConstBoard(board);
-          User2Turn(board);
-  x=analyzeboard(board)
-  if(x==0):
-    ConstBoard(board);
-    print("DRAW");
-  if(x==-1):
-    ConstBoard(board);
-    print("PLayer X wins ");
-  if(x==1):
-    ConstBoard(board);
-    print("Player O wins");
+            for i in range(0, 9):
+                if (analyzeboard(board) != 0):
+                    break;
+                if (i % 2 == 0):
+                    CompTurn(board);
+                else:
+                    ConstBoard(board);
+                    User2Turn(board);
+    x = analyzeboard(board)
+    if (x == 0):
+        ConstBoard(board);
+        print("DRAW");
+    if (x == -1):
+        ConstBoard(board);
+        print("PLayer X wins ");
+    if (x == 1):
+        ConstBoard(board);
+        print("Player O wins");
+
+main()
